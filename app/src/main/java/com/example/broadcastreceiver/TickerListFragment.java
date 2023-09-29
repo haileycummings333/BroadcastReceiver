59f1d2f66f13f1d93e613bf08a2374312803abcf
haileycummings <haileycummings333@gmail.com>
2023-09-29 10:08:30 -0500

    Broadcast Receiver

--- app/src/main/java/com/example/broadcastreceiver/TickerListFragment.java
+++ app/src/main/java/com/example/broadcastreceiver/TickerListFragment.java
@@ -7,7 +7,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+/*
+    Fragment that displays the list of ticker symbols
+    Starts with 3 default entries E.g., {BAC, AAPL, DIS}
+    Clicking on an entry in the list, opens the SeekingAlpha information website for
+        that selected ticker on the InfoWebFragment
+    The List fragment will contain no more than to 6 entries
+    Receiving any additional tickers when the list has 6 entries already should replace
+        the sixth entry with the newly received ticker
+ */
 
 public class TickerListFragment extends Fragment {
 
