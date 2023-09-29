bf365cd2e3b416187ffbdc8d73ebd0c5176cc98f
haileycummings <haileycummings333@gmail.com>
2023-09-29 09:05:19 -0500

    Broadcast Receiver

---
+++ app/src/main/java/com/example/broadcastreceiver/InfoWebFragment.java
@@ -0,0 +1,34 @@
+package com.example.broadcastreceiver;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class InfoWebFragment extends Fragment {
+
+
+    public InfoWebFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_info_web, container, false);
+        return v;
+    }
+}
---
+++ app/src/main/java/com/example/broadcastreceiver/TickerListFragment.java
@@ -0,0 +1,33 @@
+package com.example.broadcastreceiver;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class TickerListFragment extends Fragment {
+
+
+    public TickerListFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_ticker_list, container, false);
+        return v;
+    }
+}
